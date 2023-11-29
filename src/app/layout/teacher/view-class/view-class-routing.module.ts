360709cbd4be4222e8f87aae7fd2dec645b8518a
Justine Edward Santos <hydxenonmain@gmail.com>
2023-11-29 14:00:31 +0800

    Add View class ui

--- src/app/layout/teacher/view-class/view-class-routing.module.ts
+++ src/app/layout/teacher/view-class/view-class-routing.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewClassComponent } from './view-class.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { PeopleComponent } from './people/people.component';
+import { GradesComponent } from './grades/grades.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: ViewClassComponent
+    component: ViewClassComponent,
+
+    children: [
+      {
+        path: "assignments",
+        component: AssignmentsComponent
+      },
+      {
+        path: "people",
+        component: PeopleComponent
+      },
+      {
+        path: "grades",
+        component: GradesComponent
+      }
+    ]
   }
 ]
 
--- src/app/layout/teacher/view-class/view-class.module.ts
+++ src/app/layout/teacher/view-class/view-class.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewClassComponent } from './view-class.component';
 import { ViewClassRoutingModule } from './view-class-routing.module';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { PeopleComponent } from './people/people.component';
+import { GradesComponent } from './grades/grades.component';
 
 
 
 @NgModule({
   declarations: [
-    ViewClassComponent
+    ViewClassComponent,
+    AssignmentsComponent,
+    PeopleComponent,
+    GradesComponent
   ],
   imports: [
     CommonModule,
